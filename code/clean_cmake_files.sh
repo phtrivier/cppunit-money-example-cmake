cf2c1991c6154b499e64bcdcb9e28ae59ffac2b6
Pierre-Henri Trivier <phtrivier@yahoo.fr>
2009-08-28 00:13:44 +0200

    Fixed CMakeLists to look for cpp unit properly. Added 'tests' target that builds and run tests

---
+++ code/clean_cmake_files.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# I want to do only out-of-source builds. So sometimes I need to clean stuff ..
+find . -name "CMakeFiles" | xargs rm -rf
+find . -name "Makefile" | xargs rm -rf
+find . -name "cmake_install.cmake" | xargs rm -rf
+find . -name "CMakeCache.txt" | xargs rm -rf
---
+++ code/src_test/app/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/phtrivier/prj/yaraycaster/code/src_test/app
+# Build directory: /home/phtrivier/prj/yaraycaster/code/src_test/app
+# 
+# This file replicates the SUBDIRS() and ADD_TEST() commands from the source
+# tree CMakeLists.txt file, skipping any SUBDIRS() or ADD_TEST() commands
+# that are excluded by CMake control structures, i.e. IF() commands.
+ADD_TEST(tests "/home/phtrivier/prj/yaraycaster/code/bin/run_tests")
